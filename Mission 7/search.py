06495ab81fda11511518f25fc22bb791758eee79
justletmecreatetheaccount <112435002+justletmecreatetheaccount@users.noreply.github.com>
2022-11-07 19:44:00 +0100

    none

--- Mission 7/search.py
+++ Mission 7/search.py
@@ -22,5 +22,30 @@ def get_words(line):
 
 def create_index(filename):
     d = {}
-    for i in readfile(filename):
-        d[get_words(i)] = 
+    for l in readfile(filename):
+        wds = get_words(l)
+        for j in wds:
+            if j in d and readfile(filename).index(l) not in d[j]:
+                d[j].append(readfile(filename).index(l))
+            else:
+                d[j] = [readfile(filename).index(l)]
+    return d
+    
+def get_lines(words, index):
+    print (index)
+    if words[0] in index:
+        comp = index[words[0]]
+    else:
+        return []
+    pres = False
+    save = []
+    for i in comp:
+        for j in words:
+            if i in index[j]:
+                pres = True
+            else:
+                pres = False
+                break
+        if pres: 
+            save.append(i)
+    return save
